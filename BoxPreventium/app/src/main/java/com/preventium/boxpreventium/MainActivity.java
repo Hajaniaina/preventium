3dc1014ce1af212a7c584f73a13818f81123f2e6
franck-ikalogic <franck@ikalogic.com>
2016-06-01 17:35:18 +0200

    Create and test  send SMS programmatically
    Create and test  send SMS programmatically without opening message app

--- BoxPreventium/app/src/main/java/com/preventium/boxpreventium/MainActivity.java
+++ BoxPreventium/app/src/main/java/com/preventium/boxpreventium/MainActivity.java
@@ -3,6 +3,10 @@ package com.preventium.boxpreventium;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
@@ -14,9 +18,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        a = new FtpPreventium();
-        a.start();
-
+        //a = new FtpPreventium();
+        //a.start();
     }
+
+    public void sendSMS(String phoneNo, String msg) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            ArrayList<String> parts = smsManager.divideMessage(msg);
+            smsManager.sendMultipartTextMessage(phoneNo, null, parts, null, null);
+            Toast.makeText(getApplicationContext(), "Message Sent", Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),ex.getMessage().toString(), Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
 }
 
