f79c0e24cb46877e29a170d5d4475ed3aef28005
franckikalogic <franck@ikalogic.com>
2016-06-29 10:22:47 +0200

    clear

--- BoxPreventium/bluetooth/src/main/java/com/ikalogic/franck/bluetooth/device/ActionCallback.java
+++
@@ -1,9 +0,0 @@
-package com.ikalogic.franck.bluetooth.device;
-
-/**
- * Created by franck on 6/18/16.
- */
-public interface ActionCallback {
-    public void onSuccess(Object data);
-    public void onFail(int errorCode, String msg);
-}
