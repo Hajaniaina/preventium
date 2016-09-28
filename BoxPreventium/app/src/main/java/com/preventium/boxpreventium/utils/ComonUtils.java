6f1bcd4bf1730a9c798255285c9a37bee65e59cd
franckikalogic <franck@ikalogic.com>
2016-09-28 10:57:02 +0200

    Correction getIMEI() fonction

--- BoxPreventium/app/src/main/java/com/preventium/boxpreventium/utils/ComonUtils.java
+++ BoxPreventium/app/src/main/java/com/preventium/boxpreventium/utils/ComonUtils.java
@@ -13,6 +13,8 @@ import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.RequiresPermission;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 /**
  * Created by Franck on 21/09/2016.
@@ -131,9 +133,55 @@ public class ComonUtils {
 
     // === PHONE
 
-    public static String getIMEInumber(Context ctx) {
-        return Settings.Secure.getString(ctx.getContentResolver(), Settings.Secure.ANDROID_ID).toUpperCase();
+    public static void printFhoneInfo(Context ctx){
+        //Get the instance of TelephonyManager
+        TelephonyManager tm=(TelephonyManager)ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        //Calling the methods of TelephonyManager the returns the information
+        String IMEINumber=tm.getDeviceId();
+        String subscriberID=tm.getDeviceId();
+        String SIMSerialNumber=tm.getSimSerialNumber();
+        String networkCountryISO=tm.getNetworkCountryIso();
+        String SIMCountryISO=tm.getSimCountryIso();
+        String softwareVersion=tm.getDeviceSoftwareVersion();
+        String voiceMailNumber=tm.getVoiceMailNumber();
+        //Get the phone type
+        String strphoneType="";
+        int phoneType=tm.getPhoneType();
+        switch (phoneType)
+        {
+            case (TelephonyManager.PHONE_TYPE_CDMA):
+                strphoneType="CDMA";
+                break;
+            case (TelephonyManager.PHONE_TYPE_GSM):
+                strphoneType="GSM";
+                break;
+            case (TelephonyManager.PHONE_TYPE_NONE):
+                strphoneType="NONE";
+                break;
+        }
+        //getting information if phone is in roaming
+        boolean isRoaming=tm.isNetworkRoaming();
+        String info="Phone Details:\n";
+        info+="\n IMEI Number:"+IMEINumber;
+        info+="\n SubscriberID:"+subscriberID;
+        info+="\n Sim Serial Number:"+SIMSerialNumber;
+        info+="\n Network Country ISO:"+networkCountryISO;
+        info+="\n SIM Country ISO:"+SIMCountryISO;
+        info+="\n Software Version:"+softwareVersion;
+        info+="\n Voice Mail Number:"+voiceMailNumber;
+        info+="\n Phone Network Type:"+strphoneType;
+        info+="\n In Roaming? :"+isRoaming;
+        Log.d(TAG,info);
     }
+
+    public static String getIMEInumber(Context ctx) {
+        //Get the instance of TelephonyManager
+        TelephonyManager tm = (TelephonyManager)ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm.getDeviceId();
+        //return Settings.Secure.getString(ctx.getContentResolver(), Settings.Secure.ANDROID_ID).toUpperCase();
+    }
+
+
 
     // === APK
 
