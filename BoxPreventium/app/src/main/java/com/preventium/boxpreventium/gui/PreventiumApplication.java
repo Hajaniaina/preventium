b38129ae583ceb2671c84ff2fbde7e0d8c105ba0
Kosinov Vladislav <vladislav@ikalogic.com>
2016-12-13 18:02:51 +0100

    Add crash report functionality

---
+++ BoxPreventium/app/src/main/java/com/preventium/boxpreventium/gui/PreventiumApplication.java
@@ -0,0 +1,18 @@
+package com.preventium.boxpreventium.gui;
+
+import android.app.Application;
+import com.devs.acr.AutoErrorReporter;
+
+public class PreventiumApplication extends Application {
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+
+        AutoErrorReporter.get(this)
+                .setEmailAddresses("franck@ikalogic.com")
+                .setEmailSubject("Preventium Crash Report")
+                .start();
+    }
+}
