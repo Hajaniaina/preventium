f904fe6235f5b8c625dca17de49428d758bb8789
Kosinov Vladislav <vladislav@ikalogic.com>
2017-01-23 15:48:45 +0100

    Add BugShaker feedback tool

--- BoxPreventium/app/src/main/java/com/preventium/boxpreventium/gui/PreventiumApplication.java
+++ BoxPreventium/app/src/main/java/com/preventium/boxpreventium/gui/PreventiumApplication.java
@@ -2,6 +2,8 @@ package com.preventium.boxpreventium.gui;
 
 import android.app.Application;
 import com.devs.acr.AutoErrorReporter;
+import com.github.stkent.bugshaker.BugShaker;
+import com.github.stkent.bugshaker.flow.dialog.AlertDialogType;
 
 public class PreventiumApplication extends Application {
 
@@ -13,6 +15,15 @@ public class PreventiumApplication extends Application {
         AutoErrorReporter.get(this)
                 .setEmailAddresses("franck@ikalogic.com")
                 .setEmailSubject("Preventium Crash Report")
+                .start();
+
+        BugShaker.get(this)
+                .setEmailAddresses("franck@ikalogic.com")
+                .setEmailSubjectLine("Preventium Feedback")
+                .setAlertDialogType(AlertDialogType.NATIVE)
+                .setLoggingEnabled(true)
+                .setIgnoreFlagSecure(true)
+                .assemble()
                 .start();
     }
 }
