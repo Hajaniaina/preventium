c29a3d185fc2aa342cdf6829df2e5338a8764da6
Kosinov Vladislav <vladislav@ikalogic.com>
2017-04-07 09:53:44 +0200

    Add enchenced audio management to WebView

--- BoxPreventium/app/src/main/java/com/preventium/boxpreventium/gui/WebViewActivity.java
+++ BoxPreventium/app/src/main/java/com/preventium/boxpreventium/gui/WebViewActivity.java
@@ -1,20 +1,18 @@
 package com.preventium.boxpreventium.gui;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.graphics.drawable.Icon;
-import android.net.Uri;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import com.github.clans.fab.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.webkit.DownloadListener;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
-
 import com.preventium.boxpreventium.R;
 import com.preventium.boxpreventium.location.CustomMarkerData;
 
@@ -33,6 +31,7 @@ public class WebViewActivity extends AppCompatActivity {
     private int currPageIndex = 0;
     private int pageLoadProgress = 0;
     private int pageReloadAttempts = 0;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -47,16 +46,19 @@ public class WebViewActivity extends AppCompatActivity {
         ArrayList<String> urlList = new ArrayList<>();
 
         urlList.add("http://www.orimi.com/pdf-test.pdf");
-        urlList.add("http://www.hubharp.com/web_sound/HarrisLilliburleroShort.mp3");
-        urlList.add("https://www.w3schools.com/css/trolltunga.jpg");
+        urlList.add("http://techslides.com/demos/sample-videos/small.mp4");
         urlList.add("https://allthingsaudio.wikispaces.com/file/view/Out%20on%20the%20road.mp3/139234925/Out%20on%20the%20road.mp3");
-        urlList.add("https://www.youtube.com/");
+        urlList.add("https://www.w3schools.com/css/trolltunga.jpg");
+        urlList.add("https://www.ikalogic.com/");
 
         markerData.alertAttachments = urlList;
 
         ///////////////////////////////////////////////////////////////
 
-        final AppColor appColor = new AppColor(this);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        final AppColor appColor = new AppColor(getApplicationContext());
 
         webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -68,7 +70,7 @@ public class WebViewActivity extends AppCompatActivity {
         webView.getSettings().setDisplayZoomControls(true);
 
         final ProgressDialog progressBar = new ProgressDialog(this, R.style.InfoDialogStyle);
-        // progressBar.setMessage(getString(R.string.progress_loading_string));
+        progressBar.setMessage(getString(R.string.progress_loading_string));
         progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         progressBar.setCancelable(false);
         progressBar.setMax(100);
@@ -118,11 +120,11 @@ public class WebViewActivity extends AppCompatActivity {
 
                     if (pageLoadProgress < 100) {
 
-                        if (pageReloadAttempts < 3) {
+                        if (pageReloadAttempts < 5) {
 
                             pageReloadAttempts++;
-                            view.clearCache(true);
-                            view.reload();
+                            webView.clearCache(true);
+                            openPage(currPageIndex);
                         }
                     }
                 }
@@ -131,20 +133,12 @@ public class WebViewActivity extends AppCompatActivity {
             }
         });
 
-        webView.setDownloadListener(new DownloadListener() {
-
-            @Override
-            public void onDownloadStart (String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
-
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
-            }
-        });
-
         openPage(currPageIndex);
 
-        final FloatingActionButton buttonNext = (FloatingActionButton) findViewById(R.id.fab_file_check);
+        final FloatingActionButton buttonNext = (FloatingActionButton) findViewById(R.id.fab_page_next);
+        final FloatingActionButton buttonPrev = (FloatingActionButton) findViewById(R.id.fab_page_prev);
+        buttonPrev.setEnabled(false);
+
         buttonNext.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -152,9 +146,10 @@ public class WebViewActivity extends AppCompatActivity {
 
                 if (currPageIndex < (maxPages - 1)) {
 
+                    buttonPrev.setEnabled(true);
+
                     currPageIndex++;
                     pageReloadAttempts = 0;
-
                     openPage(currPageIndex);
                     updateStepView();
                 }
@@ -165,8 +160,34 @@ public class WebViewActivity extends AppCompatActivity {
 
                 if (currPageIndex >= (maxPages - 1)) {
 
-                    buttonNext.setImageIcon(Icon.createWithResource(WebViewActivity.this, R.drawable.ic_check));
+                    buttonNext.setImageIcon(Icon.createWithResource(getApplicationContext(), R.drawable.ic_check));
                     buttonNext.setColorNormal(appColor.getColor(AppColor.GREEN));
+                }
+            }
+        });
+
+        buttonPrev.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View view) {
+
+                if (currPageIndex > 0) {
+
+                    currPageIndex--;
+                    pageReloadAttempts = 0;
+                    openPage(currPageIndex);
+                    updateStepView();
+
+                    if (buttonNext.getColorNormal() == appColor.getColor(AppColor.GREEN)) {
+
+                        buttonNext.setImageIcon(Icon.createWithResource(getApplicationContext(), R.drawable.ic_chevron_right));
+                        buttonNext.setColorNormal(appColor.getColor(AppColor.BLUE));
+                    }
+
+                    if (currPageIndex == 0) {
+
+                        buttonPrev.setEnabled(false);
+                    }
                 }
             }
         });
@@ -179,7 +200,8 @@ public class WebViewActivity extends AppCompatActivity {
 
                 pageReloadAttempts = 0;
                 webView.clearCache(true);
-                webView.reload();
+                openPage(currPageIndex);
+                updateStepView();
             }
         });
     }
@@ -192,12 +214,19 @@ public class WebViewActivity extends AppCompatActivity {
         webView.setVisibility(View.GONE);
         webView.destroy();
 
+        mediaPlayer.reset();
+        mediaPlayer.release();
+        mediaPlayer = null;
+
         super.onPause();
     }
 
     public void openPage (int pageIndex) {
 
-        String url = markerData.alertAttachments.get(currPageIndex);
+        String url = markerData.alertAttachments.get(pageIndex);
+
+        mediaPlayer.stop();
+        mediaPlayer.reset();
 
         if (url.endsWith(".pdf")) {
 
@@ -205,8 +234,28 @@ public class WebViewActivity extends AppCompatActivity {
             url = "https://docs.google.com/gview?embedded=true&url=" + temp;
         }
 
+        if (isAudioStream(url)) {
+
+            try {
+
+                mediaPlayer.setDataSource(url);
+                mediaPlayer.prepare();
+            }
+            catch (IOException e) {
+
+                Log.d(TAG, "Media Player Exception: " + e.toString());
+            }
+
+            String page = "<html><body><center><img src=\"http://gurmeet.net/Images/index_page/happy-songs.png\" alt=\"Message Audio\"></center></body></html>";
+            webView.loadData(page, "text/html", "UTF-8");
+
+            mediaPlayer.start();
+            return;
+        }
+
         try {
 
+            webView.loadUrl("about:blank");
             webView.loadUrl(url);
         }
         catch (Exception e) {
@@ -215,9 +264,23 @@ public class WebViewActivity extends AppCompatActivity {
         }
     }
 
-    public void updateStepView() {
+    private void updateStepView() {
 
         String text = (currPageIndex + 1) + "/" + maxPages;
         stepsView.setText(text);
+    }
+
+    private boolean isAudioStream (String url) {
+
+        if (url.endsWith(".mp3") ||
+            url.endsWith(".wav") ||
+            url.endsWith(".3gp") ||
+            url.endsWith(".ogg") ||
+            url.endsWith(".flac")) {
+
+            return true;
+        }
+
+        return false;
     }
 }
