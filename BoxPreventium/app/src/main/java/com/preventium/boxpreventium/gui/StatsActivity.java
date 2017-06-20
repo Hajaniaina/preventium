3bfafd1c44327501bde6cac92f676652619b249b
Kosinov Vladislav <vladislav@ikalogic.com>
2017-06-20 10:00:35 +0200

    Round score numbers

--- BoxPreventium/app/src/main/java/com/preventium/boxpreventium/gui/StatsActivity.java
+++ BoxPreventium/app/src/main/java/com/preventium/boxpreventium/gui/StatsActivity.java
@@ -99,10 +99,12 @@ public class StatsActivity extends AppCompatActivity {
         distanceView.setText(distance);
 
         float speed = StatsLastDriving.get_speed_avg(getApplicationContext()) * PositionManager.MS_TO_KMPH;
-        String avgSpeed = String.valueOf(ComonUtils.round(speed, 1)) + " km/h";
+        String speedStr = String.valueOf(ComonUtils.round(speed, 1)) + " km/h";
+        avgSpeedView.setText(getString(R.string.avg_speed_string) + ": " + speedStr);
 
-        avgSpeedView.setText(getString(R.string.avg_speed_string) + ": " + avgSpeed);
-        avgScoreView.setText(getString(R.string.avg_score_string) + ": " + String.valueOf(StatsLastDriving.get_note(getApplicationContext(), SCORE_t.FINAL)));
+        float score = StatsLastDriving.get_note(getApplicationContext(), SCORE_t.FINAL);
+        String scoreStr = String.valueOf(ComonUtils.round(score, 1));
+        avgScoreView.setText(getString(R.string.avg_score_string) + ": " + scoreStr);
 
         colors = new int[5];
 
@@ -146,11 +148,19 @@ public class StatsActivity extends AppCompatActivity {
         }
 
         chartArr[ACC_OBJ].setCenterText(objStr + "\n" + accStr);
-        chartArr[ACC_RES].setCenterText(resStr + "\n" + accStr + ":" + "\n" + String.valueOf(StatsLastDriving.get_note(getApplicationContext(), SCORE_t.ACCELERATING)));
+        score = StatsLastDriving.get_note(getApplicationContext(), SCORE_t.ACCELERATING);
+        scoreStr = String.valueOf(ComonUtils.round(score, 1));
+        chartArr[ACC_RES].setCenterText(resStr + "\n" + accStr + ":" + "\n" + scoreStr);
+
         chartArr[BRK_OBJ].setCenterText(objStr + "\n" + brakeStr);
-        chartArr[BRK_RES].setCenterText(resStr + "\n" + brakeStr + ":" + "\n" + String.valueOf(StatsLastDriving.get_note(getApplicationContext(), SCORE_t.BRAKING)));
+        score = StatsLastDriving.get_note(getApplicationContext(), SCORE_t.BRAKING);
+        scoreStr = String.valueOf(ComonUtils.round(score, 1));
+        chartArr[BRK_RES].setCenterText(resStr + "\n" + brakeStr + ":" + "\n" + scoreStr);
+
         chartArr[CRN_OBJ].setCenterText(objStr + "\n" + cornerStr);
-        chartArr[CRN_RES].setCenterText(resStr + "\n" + cornerStr + ":" + "\n" + String.valueOf(StatsLastDriving.get_note(getApplicationContext(), SCORE_t.CORNERING)));
+        score = StatsLastDriving.get_note(getApplicationContext(), SCORE_t.CORNERING);
+        scoreStr = String.valueOf(ComonUtils.round(score, 1));
+        chartArr[CRN_RES].setCenterText(resStr + "\n" + cornerStr + ":" + "\n" + scoreStr);
 
         for (int i = 0; i < 6; i++) {
 
